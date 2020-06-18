bf277b04ba77bdae201eeaa1a0afaf6623f8d938
arpitgupta30 <arpitofficial123@gmail.com>
2020-06-18 16:07:46 +0530

    gitignore is working

--- myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/arpit/Ironman/Project and Resume/Weather app/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
